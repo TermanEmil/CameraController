4ccad3895c82ad216fccfe9457156af17d168b5f
unicornslayer <unicornslayer@macbookprocmm.dyndns.cern.ch>
2019-07-04 12:46:57 +0200

    removed stuff

--- Camera.py
+++
@@ -1,9 +0,0 @@
-import gphoto2 as gp
-
-
-class Camera:
-    def __init__(self, gp_camera, name='camera0'):
-        assert isinstance(gp_camera, gp.Camera)
-
-        self.gp_camera = gp_camera
-        self.name = name
