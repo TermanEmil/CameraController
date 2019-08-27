17ea053df2ac5ca29d9918973dab428ed67c9281
Emil Terman <terman.emil@gmail.com>
2019-08-27 16:26:38 +0200

    sync scripts

--- scripts/continuously_move_files.sh
+++ scripts/continuously_move_files.sh
@@ -5,10 +5,22 @@
 
 
 PATH_TO_WATCH='./Timelapses/'
-PATH_TO_MOVE_FILES_TO='/Mounted/Cern/Timelapse/Software/'
+PATH_TO_MOVE_FILES_TO='/Mounted/Cern/Timelapse/Software/Timelapses'
 
-
-inotifywait -m ${PATH_TO_WATCH} -e create -e moved_to |
+inotifywait -r -m ${PATH_TO_WATCH} -e close_write -e moved_to |
     while read path action file; do
-        echo "The file '$file' appeared in directory '$path' via '$action'"
+        echo "The file '$file' appeared in directory '$path' via '$action'";
+	
+	{ # try
+	    rsync -rvh ${PATH_TO_WATCH} ${PATH_TO_MOVE_FILES_TO} --remove-source-files;
+	} || { # catch
+	    echo 'Failed to sync folders' >&2;
+	    echo 'Trying to mount...';
+	
+	    { # try
+	    	./scripts/mount_nfs.sh;
+	    } || { # catch
+		echo 'Failed to mount' >&2;
+	    }
+	}
     done
--- scripts/mount_nfs.sh
+++ scripts/mount_nfs.sh
@@ -3,4 +3,5 @@
 DEVICE='128.141.50.125:aps'
 DIR_TO_MOUNT='/Mounted/Cern/'
 
+mkdir -p $DIR_TO_MOUNT
 mount $DEVICE $DIR_TO_MOUNT
