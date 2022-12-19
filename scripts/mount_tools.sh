0848ed8f041b8eababa68ccb9e97d028443a234b
TermanEmil <terman.emil@gmail.com>
2022-12-20 00:32:03 +0100

    Fix mounting

--- scripts/mount_tools.sh
+++ scripts/mount_tools.sh
@@ -21,6 +21,6 @@ function do_the_mounting() {
     mkdir -p ${MOUNT_POINT}
 
     if ! is_mounted; then
-        mount -v -t nfs ${MOUNT_REMOTE_NODE} ${MOUNT_POINT} && echo "Successfully mounted ${MOUNT_REMOTE_NODE} to ${MOUNT_POINT}";
+        sudo mount -v -t nfs ${MOUNT_REMOTE_NODE} ${MOUNT_POINT} && echo "Successfully mounted ${MOUNT_REMOTE_NODE} to ${MOUNT_POINT}";
     fi
 }
